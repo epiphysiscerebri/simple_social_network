26efb49fe8f39ae196fac850a1df493e9a99c059
epiphysis cerebri <zamurievanton@inbox.ru>
2022-01-18 18:57:36 +0300

    added NavLinks

--- src/App.js
+++ src/App.js
@@ -1,32 +1,29 @@
-
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar.jsx";
 import Header from "./components/Header/Header.jsx";
 import Profile from "./components/Profile/Profile.jsx";
 import Dialogs from "./components/Dialogs/Dialogs.jsx";
-import News from "./components/News/News.jsx"
-import Music from "./components/Music/Music.jsx"
-import Sett from "./components/Sett/Sett.jsx"
-import {Routes, Route, BrowserRouter} from 'react-router-dom';
+import News from "./components/News/News.jsx";
+import Music from "./components/Music/Music.jsx";
+import Sett from "./components/Sett/Sett.jsx";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 
 const App = () => {
   return (
     <BrowserRouter>
-    <div className="app-wrapper">
-      <Header />
-      <Navbar />
-      <div className="app-wrapper-content">
-      <Routes>
-        <Route path="/dialogs" element={<Dialogs/>} />
-        <Route path="/profile" element={<Profile/>} />
-        <Route path="/news" element={<News/>} />
-        <Route path="/music" element={<Music/>} />
-        <Route path="/settings" element={<Sett/>} />
-        </Routes>
-        {/* <Profile /> */}
-        {/* <Dialogs /> */}
+      <div className="app-wrapper">
+        <Header />
+        <Navbar />
+        <div className="app-wrapper-content">
+          <Routes>
+            <Route path="/dialogs" element={<Dialogs />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/news" element={<News />} />
+            <Route path="/music" element={<Music />} />
+            <Route path="/settings" element={<Sett />} />
+          </Routes>
+        </div>
       </div>
-    </div>
     </BrowserRouter>
   );
 };
--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,8 +1,24 @@
+import classes from './Dialogs.module.css';
 
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={classes.dialogs}>
+            <div className={classes.dialogsItem}>
+                <div className={classes.dialog + ' ' + classes.active}>
+                    Anton
+                </div>
+                <div className={classes.dialog}>
+                    Alexey
+                </div>
+                <div className={classes.dialog}>
+                    Andrey
+                </div>
+            </div>
+            <div className={classes.messages}>
+                <div className={classes.message}>Hi</div>
+                <div className={classes.message}>Hello</div>
+                <div className={classes.message}>Hello world</div>
+            </div>
         </div>
     )
 }
